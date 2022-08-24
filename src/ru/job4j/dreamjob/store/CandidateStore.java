48b38218e700a5a036a8016f783eff3c0cfe781e
kavalerov25 <paradise_777@mail.ru>
2022-08-24 20:54:38 +0300

    5. Создания и редактирования кандидатов. [#504858]

--- src/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/ru/job4j/dreamjob/store/CandidateStore.java
@@ -3,19 +3,22 @@ package ru.job4j.dreamjob.store;
 import ru.job4j.dreamjob.model.Candidate;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
     private static final CandidateStore INST = new CandidateStore();
+    private final AtomicInteger idCount = new AtomicInteger();
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private CandidateStore() {
-        candidates.put(1, new Candidate(1, "kirill", "стажер", LocalDateTime.now()));
-        candidates.put(2, new Candidate(2, "stas", "coach java", LocalDateTime.now()));
-        candidates.put(3, new Candidate(3, "petr", "Java senior", LocalDateTime.now()));
+        candidates.put(idCount.incrementAndGet(), new Candidate(idCount.get(), "kirill", "intern", LocalDateTime.of(2022, Month.AUGUST,24,20,40)));
+        candidates.put(idCount.incrementAndGet(), new Candidate(idCount.get(), "stas", "coach java", LocalDateTime.now()));
+        candidates.put(idCount.incrementAndGet(), new Candidate(idCount.get(), "petr", "Java senior", LocalDateTime.now()));
     }
 
     public static CandidateStore instOf() {
@@ -24,5 +27,18 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public void add(Candidate candidate) {
+        candidate.setId(idCount.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    public Object findById(int id) {
+        return candidates.get(id);
+    }
+
+    public void update(Candidate candidate) {
+        candidates.put(candidate.getId(), candidate);
     }
 }
