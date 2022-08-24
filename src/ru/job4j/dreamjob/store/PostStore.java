ea7094eca1eeff0a9889d26872306ca5e7bbf553
kavalerov25 <paradise_777@mail.ru>
2022-08-24 12:21:31 +0300

    3. PostController.createPost. Создание вакансии. [#504849] fix

--- src/ru/job4j/dreamjob/store/PostStore.java
+++ src/ru/job4j/dreamjob/store/PostStore.java
@@ -6,16 +6,17 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
     private static final PostStore INST = new PostStore();
-
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    AtomicInteger idCount = new AtomicInteger();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "java developer", LocalDate.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "java developer", LocalDate.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "java developer", LocalDate.now()));
+        posts.put(idCount.incrementAndGet(), new Post(idCount.get(), "Junior Java Job", "java developer", LocalDate.now()));
+        posts.put(idCount.incrementAndGet(), new Post(idCount.get(), "Middle Java Job", "java developer", LocalDate.now()));
+        posts.put(idCount.incrementAndGet(), new Post(idCount.get(), "Senior Java Job", "java developer", LocalDate.now()));
     }
 
     public static PostStore instOf() {
@@ -27,6 +28,7 @@ public class PostStore {
     }
 
     public void add(Post post) {
+        post.setId(idCount.incrementAndGet());
         posts.put(post.getId(), post);
     }
 }
