3d11d14ce291275166ce0c5605f28f5991336b09
kavalerov25 <paradise_777@mail.ru>
2022-08-25 11:59:18 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.store;
 
+import net.jcip.annotations.ThreadSafe;
 import ru.job4j.dreamjob.model.Candidate;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class CandidateStore {
     private final AtomicInteger idCount = new AtomicInteger();
--- src/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/java/ru/job4j/dreamjob/store/PostStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.store;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Post;
 
@@ -8,6 +9,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class PostStore {
 
@@ -20,10 +22,6 @@ public class PostStore {
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
 
-    }
-
-    public static PostStore instOf() {
-        return INST;
     }
 
     public Collection<Post> findAll() {
