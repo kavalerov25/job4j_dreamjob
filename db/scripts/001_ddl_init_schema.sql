6decba2ef1419c6eed2b1b948deb28c63980bf81
kavalerov25 <paradise_777@mail.ru>
2022-09-10 10:46:12 +0300

    4. Многопоточность в базе данных [#504860]

--- db/scripts/001_ddl_init_schema.sql
+++ db/scripts/001_ddl_init_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS post (
+create TABLE IF NOT EXISTS post (
     id          SERIAL PRIMARY KEY,
     name        TEXT,
     description TEXT,
@@ -7,10 +7,17 @@ CREATE TABLE IF NOT EXISTS post (
     city_id     INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS candidate (
+create TABLE IF NOT EXISTS candidate (
     id          SERIAL PRIMARY KEY,
     name        TEXT,
     description TEXT,
     created     TIMESTAMP,
     photo       BYTEA
+);
+
+create TABLE IF NOT EXISTS users (
+  id            SERIAL PRIMARY KEY,
+  name     VARCHAR(50),
+  email    VARCHAR(50) UNIQUE,
+  password TEXT
 );
---
+++ src/main/java/ru/job4j/dreamjob/model/User.java
@@ -0,0 +1,76 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return getId() == user.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+               + "id=" + id
+               + ", name='" + name + '\''
+               + ", email='" + email + '\''
+               + ", password='" + password + '\''
+               + '}';
+    }
+}
