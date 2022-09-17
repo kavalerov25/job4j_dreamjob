fcfeaa2736568bccaa0cb3ff851811d0b0bae841
kavalerov25 <paradise_777@mail.ru>
2022-09-17 17:59:30 +0300

    3. Filter [#504865 #342975]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -4,16 +4,23 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 public class AuthFilter implements Filter {
+
+    private final Set<String> uriSet = Set.of("/loginPage", "/login", "/formAddUser",
+            "/registration", "/fail", "/success");
+
+    private boolean checkUri(String uri, Set<String> uriSet) {
+        return uriSet.contains(uri);
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")
-            || uri.endsWith("fail") || uri.endsWith("success"))  {
+        if (checkUri(uri, uriSet)) {
             filterChain.doFilter(req, res);
             return;
         }
